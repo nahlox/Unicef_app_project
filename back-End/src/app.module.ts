e8bfd52a236d943ecfe6c3215d86bba24270aab5
Rawad Nahle <rawad.nahle10@gmail.com>
2022-03-20 20:19:56 +0100

    update

--- back-End/src/app.module.ts
+++ back-End/src/app.module.ts
@@ -3,18 +3,18 @@ import {ConfigModule} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
+import { UserModule } from './user/user.module';
+import { QuestionsModule } from './questions/questions.module';
+import { RolesModule } from './roles/roles.module';
+import { RegistrationsModule } from './registrations/registrations.module';
+import { EventsModule } from './events/events.module';
+import { DocumentsModule } from './documents/documents.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    type : 'postgres',
-    database: 'db',
-    entities: ['/src/**/*.entity.ts'],
-    username: 'rawad',
-    password: 'test',
-    port: 5432,
-    synchronize: true,
 
-   })],
+   }), UserModule, QuestionsModule, RolesModule, RegistrationsModule, EventsModule, DocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
