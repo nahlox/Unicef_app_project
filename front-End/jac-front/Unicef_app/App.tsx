64bb6922a575dc671a1e3bc17a4b92f0795bb7cf
Rawad Nahle <rawad.nahle10@gmail.com>
2022-03-20 19:17:02 +0100

    new

--- front-End/jac-front/Unicef_app/App.tsx
+++ front-End/jac-front/Unicef_app/App.tsx
@@ -1,20 +1,39 @@
-import React from "react-native";
+
 import { StatusBar } from "expo-status-bar";
 
 //screens
-import Login from "./screens/loginPage";
+import LoginScreen from "./screens/Login";
+import SignUpScreen from "./screens/SignUp";
+import ForgotPasswordScreen from "./screens/ForgotPassword";
+import AccountScreen from "./screens/Account";
+import FAQScreen from "./screens/FAQ";
+import EventsScreen from "./screens/Events";
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabNavigator from "./navigation/BottomTab";
+import NewPasswordScreen from "./screens/Newpassword";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return(
-    <Login
-    
-    /> 
+    <NavigationContainer >
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+     
+      
+      <Stack.Screen name="BottomTab" component={BottomTabNavigator}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
 
 
   );
 
-}
-
-
+};
 
 
